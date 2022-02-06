222dfa7436c177b02a9957c553c73879b96cf933
kilokid <arlomov007@gmail.com>
2022-02-06 15:30:06 +0300

    slider scripts

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,14 +1,46 @@
 export const slider = () =>
 {
-	const slidesField = document.querySelector('.slider-content');
-	const slides = document.querySelectorAll('.slide');
-	const slidesDescription = document.querySelectorAll('.slide-description');
-	const btnPrev = document.getElementById('btn-prev');
-	const btnNext = document.getElementById('btn-next');
-	let slideWidth = 880;
-
+	const slider = document.querySelector('.slider');
+	const slidesField = slider.querySelector('.slider-content');
+	const slides = slider.querySelectorAll('.slide');
+	const slide = slider.querySelector('.slide');
+	const slidesDescription = slider.querySelectorAll('.slide-description');
+	const btnPrev = slider.querySelector('.btn-prev');
+	const btnNext = slider.querySelector('.btn-next');
 	let slideIndex = 1;
 	let offset = 0;
+
+	const slidesFieldTransform = () =>
+	{
+		if ( document.body.clientWidth <= 1200 )
+		{
+			slidesField.style.width = `${100 * slides.length}%`;
+
+			slidesField.style.transform = `translateX(-${slide.getBoundingClientRect().width}px)`;
+		}
+		else
+		{
+			slidesField.style.width = `${50 * slides.length}%`;
+		}
+	}
+
+	slidesFieldTransform();
+
+	const debounce = ( func, time = 100 ) =>
+	{
+		let timer;
+		return function( event )
+		{
+			if( timer )
+			{
+				clearTimeout( timer );
+			}
+			timer = setTimeout( func, time, event );
+		};
+	}
+
+	window.addEventListener( "resize", debounce( slidesFieldTransform, 150 ) );
+
 
 	const showSlide = ( i = 1 ) =>
 	{
@@ -39,28 +71,32 @@ export const slider = () =>
 	hideSlide();
 	showSlide(slideIndex);
 
-	slidesField.style.width = 45 * slides.length + '%';
-
-	slides.forEach( slide =>
-		{
-			slide.style.width = `${slideWidth}px`;
-		}
-	);
-
 	btnNext.addEventListener( 'click', () =>
 		{
+			const actualSlideWidth = slide.getBoundingClientRect().width;
+
 			hideSlide();
 			showSlide(++slideIndex);
-			offset = slideWidth * slideIndex * - 1 + slideWidth;
+
+			offset = document.body.clientWidth <= 1200 ?
+				actualSlideWidth * slideIndex * - 1 :
+				actualSlideWidth * slideIndex * - 1 + actualSlideWidth;
+
 			slidesField.style.transform = `translateX(${offset}px)`;
 		}
 	);
 
 	btnPrev.addEventListener( 'click', () =>
 		{
+			const actualSlideWidth = slide.getBoundingClientRect().width;
+
 			hideSlide();
 			showSlide(--slideIndex);
-			offset = slideWidth * slideIndex * -1 + slideWidth;
+
+			offset = document.body.clientWidth <= 1200 ?
+				actualSlideWidth * slideIndex * - 1 :
+				actualSlideWidth * slideIndex * - 1 + actualSlideWidth;
+
 			slidesField.style.transform = `translateX(${offset}px)`;
 		}
 	);
